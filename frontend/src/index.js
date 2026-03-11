5e5cd0ef726cbc890e05cc0359b5759ceefab81e
Sk Fahimuddin <skf4988@gmail.com>
2026-03-12 04:01:08 +0530

    axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import App from './App';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || '';
+
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
